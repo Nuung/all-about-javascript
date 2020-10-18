a45fb34af9a601c601439907bb00c041f2fe6d24
Nuung <qlgks1@naver.com>
2020-10-18 23:35:27 +0900

    default index html css change

--- NomadJS/todo.js
+++ NomadJS/todo.js
@@ -29,8 +29,8 @@ function paintToDo(text) {
   delBtn.innerText = "❌";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
-  li.appendChild(delBtn);
   li.appendChild(span);
+  li.appendChild(delBtn);
   li.id = newId;
   toDoList.appendChild(li);
   const toDoObj = {
