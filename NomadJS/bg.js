3031e291adf22a6b41b83d6245b91d315fc304ee
Nuung <qlgks1@naver.com>
2020-10-18 23:00:56 +0900

    change image dir

--- NomadJS/bg.js
+++ NomadJS/bg.js
@@ -4,7 +4,7 @@ const IMG_NUMBER = 3;
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `images/${imgNumber + 1}.jpg`;
+  image.src = `./NomadJS/images/${imgNumber + 1}.jpg`;
   image.classList.add("bgImage");
   body.prepend(image);
 }
