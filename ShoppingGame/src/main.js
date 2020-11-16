9863962cf1bb5e8a5afd34b27872476ce129b53f
Nuung <qlgks1@naver.com>
2020-11-17 01:26:43 +0900

    shopping game main.js and data.json create

--- ShoppingGame/src/main.js
+++ ShoppingGame/src/main.js
@@ -1,2 +1,16 @@
 'use strict';
 
+function loadItems() {
+    return fetch('./ data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+
+// main -> promise가 전달해줘야행~ 
+loadItems()
+    .then(items => {
+        console.log(items);
+        // document(items);
+        // setEventListeners(items);
+    })
+    .catch(console.log);
