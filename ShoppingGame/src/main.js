36bc4f16eef588bd6cc830f92275d746f863f3f3
Nuung <qlgks1@naver.com>
2020-11-17 14:10:29 +0900

    update data and mainjs

--- ShoppingGame/src/main.js
+++ ShoppingGame/src/main.js
@@ -1,15 +1,31 @@
 'use strict';
 
 function loadItems() {
-    return fetch('./ data/data.json')
-    .then(response => response.json())
-    .then(json => json.items);
+    return fetch('https://raw.githubusercontent.com/Nuung/all-about-javascript/master/ShoppingGame/data/data.json')
+        .then(response => response.json())
+        .then(json => json.items);
+}
+
+function createHTMLString(item) {
+    // console.log(item);
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>        
+    `;
+}
+
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
 // main -> promise가 전달해줘야행~ 
 loadItems()
     .then(items => {
         console.log(items);
+        displayItems(items);
         // document(items);
         // setEventListeners(items);
     })
