a5e20eaa9eacaf0f5c671c0c7626a25791c7ca09
Nuung <qlgks1@naver.com>
2021-08-22 02:42:33 +0900

    clear noom and update css content a little

--- NomadJS-Zoom/src/public/js/app.js
+++ NomadJS-Zoom/src/public/js/app.js
@@ -25,6 +25,7 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName = "";
+let myPeerConnection;
 
 // -------------------------- Function area -------------------------- //
 
@@ -43,6 +44,7 @@ async function getMedia(deviceId) {
     }
 
     try {
+        
         // getUserMedia에 값으로 오는 것들이 constraints ( https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia )
         myStream = await navigator.mediaDevices.getUserMedia(
             deviceId ? cameraConstraints : initConstraints
@@ -117,38 +119,159 @@ function handleCameraClick() {
     }
 }
 
+// Peer 2 Peer 연결할때 작동 안하는 issuse
 async function handleCameraChange() {
-    log(camerasSelect.value);
+    // log(camerasSelect.value);
+    // 카메라가 바뀔 때 new stream이 만들어진다! 해당하는 그 stream을 update해줘야 한다! 
     await getMedia(camerasSelect.value);
+
+    // peer 2 peer connection이 되어 있을때 카메라 변경을 하면
+    // myStream이 새롭게 생겼고, 그 값 바탕으로 sender를 수정해줘야 한다!! 
+    if (myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+
+        // log(myPeerConnection.getSenders());
+        // video에 해당하는 sender만 가져오기!
+        const videoSender = myPeerConnection.getSenders()
+            .find(sender => sender.track.kind === "video");
+
+        videoSender.replaceTrack(videoTrack);
+    }
+
 }
 
-function startMedia() {
+// called by handleWelcomeSubmit
+async function initCall() {
     // display 처리 해주기
-    welcome.hidden = true;
+    // welcome.hidden = true;
+    welcome.style.display = "none";
     call.hidden = false;
-    getMedia(); // 비디오 스트리밍의 스타트 지점! 
+    await getMedia(); // 비디오 스트리밍의 스타트 지점! 
+    makeConnection();
 }
 
-function handleWelcomeSubmit(event) {
+async function handleWelcomeSubmit(event) {
     event.preventDefault();
     const input = welcomeForm.querySelector("input");
+    // welcomeForm.hidden = true;
     // log(input.value);
 
     // socket io를 통해 서버로 데이터를 보내주자 
-    socket.emit("join_room", input.value, startMedia);
+    // 다음 문제 핸들링 위해 await로 먼저 호출하고, join room에는 넘겨주지 않는 방식으로 변경
+    // -> undefined가 일어난다 -> socket io 통신이 myPeerConnection이 정의 되기 전에 offer값이 들어오기 때문에
+    await initCall();
+    socket.emit("join_room", input.value);
     roomName = input.value; // room name 저장하기!
     input.value = "";
 }
 
+// peer A <-> peer B / IceCandidate !
+function handleIce(data) {
+
+    // Icecandidate의 data들은 peer A <-> peer B 끼리 주고 받아야 한다 
+    // data를 서버를 통해 주고 받기 
+    socket.emit("ice", data.candidate, roomName);
+    log("🔴 Send icecandidate");
+    // log(data);
+}
+
+// Peer connection이 된 후에 이제 서로 스트림을 공유하기! 
+function handleAddStream(data) {
+    log("⚫️ got an stream from my peer");
+
+    // Peer A와 Peer B의 아래 값이 어떻게 다른지, 어떤 값을 서로 가지고 있는지 확인해보자!
+    log("⚫️ Target Peer's Stream:", data.stream);
+    log("⚫️ My Stream Stream:", myStream);
+
+    // 이제 서로 스트림 값 공유하니, 그 값을 DOM에 insert 해주기만 하면 된다. 
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+}
+
 // ------------------------- Socket code area -------------------------- //
-
-socket.on("welcome", () => {
+// Peer A
+socket.on("welcome", async () => {
     log("someone joined");
-})
 
+    // 누가 peer A일까? 누가 peer B일까? offer를 알아보자!
+    const offer = await myPeerConnection.createOffer();
+    // log(offer); // sdp 값이 peer 끼리 연결해주는 값! RTCsessionDescription!! 초대장!
+    myPeerConnection.setLocalDescription(offer); // 나의 peerConnection을 초대장 값으로!
+
+    // 위 오퍼 값을 peer B로 보내줘야 한다
+    // 누구한테 이걸 보낼지!
+    log("🔵 Peer A Send The Offer");
+    socket.emit("offer", offer, roomName);
+});
+
+// Peer B
+socket.on("offer", async (offer) => {
+    log("🟢 Peer B Get The Offer");
+    // log(offer);
+
+    // remote (peer B 입장에서) offer값을 set 해주기 
+    // undefined가 일어난다 -> socket io 통신이 myPeerConnection이 정의 되기 전에 offer값이 들어오기 때문에
+    myPeerConnection.setRemoteDescription(offer);
+
+    // answer 만들어줘서 peer A에게 응답해주자
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer); // Peer B의 local은 answer로 정의!!
+    // log(answer); // offer 값과 비슷하게 생겼다! 이걸 이제 Peer A에게 보내주자! 
+    socket.emit("answer", answer, roomName);
+    log("🟢 Peer B Send The Answer");
+});
+
+// Peer A
+// 위에서 peer B가 보낸 answer을 서버를 통해 받는다
+socket.on("answer", (answer) => {
+    // peer A도 이제 remote 값을 answer로 세팅해주자!! 
+    log("🔵 Peer A Get The Answer");
+    myPeerConnection.setRemoteDescription(answer);
+
+    // 이제 peer A <-> peer B 의 peer Connection이 완료, 생성 되었다.
+    // 이때부터 IceCandidate 가 일어난다
+    // IceCandidate는 webRTC에 필요한 프로토콜을 의미한다. -> 멀리 떨어진 장치와 소통할 수 있게 하기 위함이다! (프로토콜)
+
+});
+
+// Peer A <-> Peer B ice candidate data socket handle
+socket.on("ice", (ice) => {
+    log("🔴 Get the candidate");
+    myPeerConnection.addIceCandidate(ice);
+})
+// --------------------------- RTC code area ---------------------------- //
+// RTC peer 만들기
+function makeConnection() {
+    // const peerConnection = new RTCPeerConnection();
+    // 이 연결을 모든 곳에 공유하고 싶다! 그 stream을 공유한 것 처럼! 
+    // 다른 네트워크 환경에서도 사용 가능하게 (stream이 공유 가능하게) 하려면 stun 서버를 사용해야 한다!
+    // 장치에 공용 주소를 알려주는 역할이다! 서로 찾을 수 있게! 
+    // https://alnova2.tistory.com/1110
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [
+            {
+                urls: [
+                    "stun:stun.l.google.com:19302",
+                    "stun:stun1.l.google.com:19302",
+                    "stun:stun2.l.google.com:19302",
+                    "stun:stun3.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302",
+                ],
+            },
+        ],
+    });
+    // myPeer에 (peer A또는 peerB 모두) data를 주고 받는 이벤트 활성화, 그 이벤트는 icecandidate
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+    // log(myStream.getTracks()); // 이 각각(오디오, 비디오)의 트랙, 그 데이터들을 peer connection에 담아야 한다
+    // myStream obj에 해당 track을 add 해주기 -> 근데 myPeerConnection에다가 add해주기 
+    myStream
+        .getTracks()
+        .forEach(track => myPeerConnection.addTrack(track, myStream));
+}
 
 // ----------------------------- Main area ----------------------------- //
-
+// media start 핸들링 처리는 방 입장 버튼이 해줌! welcomeForm -> handleWelcomeSubmit -> 
 const init = () => {
     call.hidden = true;
 }
--- NomadJS-Zoom/src/server.js
+++ NomadJS-Zoom/src/server.js
@@ -1,6 +1,7 @@
-import http from "http";
+import http from "http";            // import HTTP, middle ware concept FRAMEWORK for node 
+import https from "https";          // for https with openssl (mobile camera issue)
+import fs from "fs";                // for openssl pem file load
 import SocketIo from "socket.io";
-// import HTTP, middle ware concept FRAMEWORK for node 
 import express from "express";
 
 // static configue
@@ -23,7 +24,11 @@ app.use("/public", express.static(__dirname + "/public"));
 /**
  * @httpRequestAndResponse
  */
-app.get("/", (_, res) => res.render("home"));
+app.get("/", (req, res) => {
+    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    log(`${new Date().toISOString()}: ${fullUrl}`);
+    res.render("home");
+});
 // app.get("/*", (req, res) => {
 //     const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 //     log(`${new Date().toISOString()}: ${fullUrl}`);
@@ -31,9 +36,14 @@ app.get("/", (_, res) => res.render("home"));
 // }); // use only home url!
 // ======================================================
 
+const httpsOption = {
+    key: fs.readFileSync("/Users/nuung/.ssh/https_private.pem"),
+    cert: fs.readFileSync("/Users/nuung/.ssh/https_public.pem")
+}
+
 // set up server listen port with log
 const handleListen = (port) => {
-    if (port) log(`http Server Run Clear with express, hello Noom world! : ws://${host}:${port}`);
+    if (port) log(`http Server Run Clear with express, hello Noom world! :  ${protocol}://${host}:${port}`);
     else log(`WebSocket Server Run Clear with http module!! hello Noom world! : ${protocol}://${host}:${defaultPort}`);
 };
 // app.listen(defaultPort, handleListen); // -> websocket 예시를 위해 주석
@@ -48,16 +58,34 @@ const handleListen = (port) => {
 // application 계증의 프로토콜, http 프로토콜 기반으로 웹소켓 서버 구축, 그래서 http 모듈 이용 
 // Create A Server
 const httpServer = http.createServer(app);
+const httpsServer = https.createServer(httpsOption, app);
 const wsServer = SocketIo(httpServer);
 
 // Socket Io request 처리 
 wsServer.on("connection", socket => {
-    socket.on("join_room", (roomName, done) => {
+    socket.on("join_room", (roomName) => {
         socket.join(roomName);
-        done();
-        socket.protocol(roomName).emit("welcome"); // FE로 welcome event (socket) 트리깅하기 
-    })
+        // done();
+        socket.to(roomName).emit("welcome"); // FE로 welcome event (socket) 트리깅하기 
+    });
+
+    // 받은 offer를 이제 필요할 peer B에게 보내줘야 함! 
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+
+    // Peer B는 offer를 받았고, 자신의 remote sds값을 받은 offer로 set했다
+    // 이제 그에 응답을 서버로 줘야했고, 아래는 그 응답을 Peer A가 받을 것이다. 
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+
+    // Icecandidate 를 통한 data, peer A <-> peer B 주고 받기 
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+    });
 });
 
 // Set up webscoket server by set port
 httpServer.listen(3003, handleListen(3003));
+// httpsServer.listen(443, handleListen(443));
